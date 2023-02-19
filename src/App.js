97cb23a3e50dfddf711f9e2ca41ce431b4d4fb8b
SolForte <mateusbrandao1234@gmail.com>
2023-02-19 11:18:23 -0300

    Palavras.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import palavras from "./palavras";
+
+const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function App() {
   return (
-    <></>
+    <>
+    <Jogo/>
+    <Letras/>
+    </>
   );
 }
 
